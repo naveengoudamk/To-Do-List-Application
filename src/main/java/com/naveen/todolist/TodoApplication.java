5f17032a1ddb71d9299edbc31595bfcb84301056
Naveengouda M K <navi112004naveen@gmail.com>
2026-08-30 08:04:11 +0530

    Add Todo application entry point

---
+++ src/main/java/com/naveen/todolist/TodoApplication.java
@@ -0,0 +1,9 @@
+package com.naveen.todolist;
+
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("To-Do List Application Started!");
+    }
+}
+
